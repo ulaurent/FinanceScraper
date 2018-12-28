0a23f660ef5ada0c9836dd69b43ad46ab06b3756
ulaurent <ulaurent12@gmail.com>
2018-12-27 21:22:25 -0500

    added them and DB construct

--- FinanceScraper/Controllers/HomeController.cs
+++ FinanceScraper/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace FinanceScraper.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Financial Scraper.";
 
             return View();
         }
